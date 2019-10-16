0d9a6a9839e42be41becb17b4543c3c490b6f217
Utilisateur <user@debian.arcahe.ovh>
2019-10-16 14:47:05 +0200

    ajout de la classe Fraction avec getters et constructeurs avec denominateur par défaut

---
+++ TD4/Fraction/Fraction/src/main/java/fr/uvsq/groupe/Fraction.java
@@ -0,0 +1,5 @@
+package fr.uvsq.groupe;
+
+public class Fraction {
+
+}
---
+++ TD4/Fraction/Fraction/src/test/java/fr/uvsq/groupe/FractionTest.java
@@ -0,0 +1,14 @@
+package fr.uvsq.groupe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FractionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
